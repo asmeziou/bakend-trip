5b96f4570d25c35ed3637543a663c12f0794ceb0
asmeziou <asmeziou@gmail.com>
2026-05-23 16:39:45 +0200

    modif Port

--- index.js
+++ index.js
@@ -50,6 +50,8 @@ app.post("/form", async (req, res) => {
   }
 });
 
-app.listen(3000, () => {
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
   console.log("Server is started ! 📧");
 });
